34facc15213cb7b66ad3fdcf487568c51ca242a6
nic-chpe <78767198+nic-chpe@users.noreply.github.com>
2021-02-25 19:24:00 -0800

    Created assignment 1 and 2 folders

---
+++ assignment2/js/01-larger-or-smaller.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("Enter first number"));
+let num2 = parseInt(prompt("Enter second number"));
+
+if(num1 > num2) {
+    alert('First number is greater: ' + num1)
+} else if (num1 < num2) {
+    alert('Second number is greater: ' + num2)
+} else {
+    alert('Both numbers are equal.')
+}
