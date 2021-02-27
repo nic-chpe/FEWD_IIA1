67d95a870cb9745455fd7fd060c17aab37f6fc5a
nic-chpe <78767198+nic-chpe@users.noreply.github.com>
2021-02-26 19:05:23 -0800

    Watched lecture and completed the excercise

--- assignment2/js/05-hitchhikers-guide.js
+++ assignment2/js/05-hitchhikers-guide.js
@@ -1,4 +1,32 @@
-let play = true;
-let play = prompt("Are you ready to play the game?");
-// Waiting to watch lecture.
+// STEP 1 
+let play = prompt("Are you ready to play the game?").toLowerCase();
+if (play == "yes") {
+    //STEP 2
+    alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.'); 
+    
+    // STEP 3
+    let direction = prompt('Enter direction').toLowerCase();
+
+    switch(direction) {
+    case 'forward':
+    alert('You walk 100 yards and safely make your way out of the cave.');
+    break;
+
+    case 'left':
+    alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall/n into a pool of water and drown!');
+    break;
+
+    case 'right':
+    alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!');
+    break;
+    
+    default : 
+    alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.');
+}
+    } else {
+    alert('Thank you, play again anytime!');
+}
+
+
+
 
