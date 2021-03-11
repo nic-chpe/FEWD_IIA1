c7ace9be43a03c6ed912d7b264efeb0da4621932
nic-chpe <78767198+nic-chpe@users.noreply.github.com>
2021-03-11 12:47:32 -0800

    rock paper scissors

---
+++ assignment4/js/02-rock-paper-scissors.js
@@ -0,0 +1,44 @@
+alert("Welcome to Rock, Paper, Scissors game!" + "\n" + "Let's play.");
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+    alert("It's a tie!");
+}
+if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+        alert("rock wins");
+    } else {
+        alert("paper wins");
+    }
+}
+if(choice1 === "paper") {
+    if(choice2 === "rock") {
+        alert("paper wins");
+    } else {
+        if(choice2 === "scissors") {
+            alert("scissors wins");
+    }
+}
+if(choice1 === "scissors") {
+    if(choice2 === "rock") {
+        alert("rock wins");
+    } else {
+        if(choice2 === "paper") {
+            alert("scissors wins");
+        }
+    }
+}
+}
+};
+console.log("User Choice: " + userChoice);
+console.log("Computer Choice: " + computerChoice);
+compare(userChoice, computerChoice);
