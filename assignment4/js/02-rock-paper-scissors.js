8e9cb1825b46c97fd576f099d192767057c54d9a
nic-chpe <78767198+nic-chpe@users.noreply.github.com>
2021-03-11 13:13:36 -0800

    leaned out the code

--- assignment4/js/02-rock-paper-scissors.js
+++ assignment4/js/02-rock-paper-scissors.js
@@ -1,43 +1,42 @@
-alert("Welcome to Rock, Paper, Scissors game!" + "\n" + "Let's play.");
+alert("Welcome to Rock, Paper, Scissors game.\nLet's play!");
+//User Choice runs first
 var userChoice = prompt("Do you choose rock, paper or scissors?");
+//Computer Choice runs second
 var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if(computerChoice <= 0.67) {
     computerChoice = "paper";
-} else {
+    } else {
     computerChoice = "scissors";
-}
-
-var compare = function(choice1, choice2) {
+    }
+//Insert function
+let compare = function(choice1, choice2) {
     if(choice1 === choice2) {
     alert("It's a tie!");
-}
-if(choice1 === "rock") {
-    if(choice2 === "scissors") {
-        alert("rock wins");
-    } else {
-        alert("paper wins");
     }
-}
-if(choice1 === "paper") {
-    if(choice2 === "rock") {
-        alert("paper wins");
-    } else {
+    
+    if(choice1 === "rock") {
         if(choice2 === "scissors") {
-            alert("scissors wins");
-    }
-}
-if(choice1 === "scissors") {
-    if(choice2 === "rock") {
-        alert("rock wins");
-    } else {
-        if(choice2 === "paper") {
-            alert("scissors wins");
+         alert("Woohoo! Rock wins.");
+            } else {
+                alert("Sorry, paper wins.");
         }
     }
-}
-}
+    if(choice1 === "paper") {
+        if(choice2 === "rock") {
+            alert("Woohoo! Paper wins.");
+            } else {
+                alert("Sorry, Scissors win.");
+        }
+    }
+    if(choice1 === "scissors") {
+        if(choice2 === "rock") {
+            alert("Sorry, rock wins.");
+            } else {
+            alert("Woohoo! Scissors win!");
+        }
+    }
 };
 console.log("User Choice: " + userChoice);
 console.log("Computer Choice: " + computerChoice);
